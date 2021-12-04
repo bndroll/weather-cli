88cb06eb69553c424c91c46326fac95c71432e5e
bounderoll <bounderoll.23@gmail.com>
2021-12-04 21:33:16 +0400

    feat: get command line arguments & start app file

---
+++ weather.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js'
+
+const initCLI = () => {
+    // const args = getArgs(process.argv)
+
+    console.log(process.argv)
+
+    /* if (args.h) {
+        // help
+    } */
+
+    /* if (args.s) {
+        // save city
+    } */
+
+    /* if (args.t) {
+        // save token
+    } */
+
+    // show weather
+}
+
+initCLI()
